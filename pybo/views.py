b6bd42162de32d2e6df604206d79b675d56ba1c9
e9e9e9 <78357762+e9e9e9@users.noreply.github.com>
2022-07-25 17:10:27 +0900

    Apply Django admin

--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 # Create your views here.
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to Django Project")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
